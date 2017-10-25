88a3a78ead5eb3596b2d618025ece83eb670a189
krisdianagus <32463039+krisdianagus@users.noreply.github.com>
2017-10-25 17:55:36 +0800

    Output Bermacam Gaya

--- praktek0202.cpp
+++ praktek0202.cpp
@@ -5,10 +5,13 @@ int main(){
 	
 	int nilai=0;
 	int Nilai=1;
-	int NiLai=2;
+	int NILAI=2;
 	int niLai=3;
 
 double angka(10.5),Angka,ANGKA(3.14);
 int a(10),b(7),c(12);
-	
+
+	cout << nilai<<" : "<<Nilai<<" | "<<NILAI<<" "<<niLai<<endl;
+	cout << angka <<"\n";
+	cout << Angka <<endl<<ANGKA;	
 }
